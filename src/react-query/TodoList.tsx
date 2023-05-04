ac5f132df4d8802190ca7aff5e3c9890d0dc50cf
allanviguilla <viguilla.allan@gmail.com>
2023-05-04 02:16:35 -0700

    Exercise: custom usePosts hook

--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -8,13 +8,16 @@ const TodoList = () => {
   if (error) return <p>{error.message}</p>;
 
   return (
-    <ul className="list-group">
-      {todos?.map((todo) => (
-        <li key={todo.id} className="list-group-item">
-          {todo.title}
-        </li>
-      ))}
-    </ul>
+    <>
+      <h3>Todos</h3>
+      <ul className="list-group">
+        {todos?.map((todo) => (
+          <li key={todo.id} className="list-group-item">
+            {todo.title}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
